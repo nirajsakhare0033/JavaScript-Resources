75003caeea04a2a9f3148df0a23108cf3815f99b
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-08-09 11:59:06 +0530

    Arrow function update

--- 1.JavaScript/ArrowFunction.js
+++ 1.JavaScript/ArrowFunction.js
@@ -9,57 +9,55 @@
 //7. Rest Operators
 //8. Spread oprators
 
-//let Keyword: 
-    // The let is used to declare variables with block scope. variables declared with let are mutable, meaning theire value can be reassigned
+//let Keyword:
+// The let is used to declare variables with block scope. variables declared with let are mutable, meaning theire value can be reassigned
 
-    //block scope
+//block scope
 
 //const Keyword:
-     // The const keyword is used to decleare variable with block scope, but ones a value is assigned to a const variable, it cannot be reassigned. const variable are immutable
+// The const keyword is used to decleare variable with block scope, but ones a value is assigned to a const variable, it cannot be reassigned. const variable are immutable
 
-     //block scope
+//block scope
 
 //Fat Arrow Function
 //const ArrowFunctionName = (parameter1, parameter2, ....) =>{}
 
-
-const sum = (a,b) => {
-  let result = `the sum of ${a} and ${b} is ${a+b}`
-  console.log(result)
-}
+const sum = (a, b) => {
+  let result = `the sum of ${a} and ${b} is ${a + b}`;
+  console.log(result);
+};
 //sum(5,5);
 
 //2 problem
 // Print numbers from 1 to 100
 const ArrowNumber = (number) => {
-  for(let i = 0; i<=number; i++){
-    console.log(i)
+  for (let i = 0; i <= number; i++) {
+    console.log(i);
   }
-}
+};
 //ArrowNumber(100)
 
 //3 prblm
 //Print even numbers between 1 and 50:
 
-const ArrowEven = (number) =>{
-  for(let i=0; i<=number; i++){
-    if(i%2==0){
-      console.log(`This is the even number ${i}`)
+const ArrowEven = (number) => {
+  for (let i = 0; i <= number; i++) {
+    if (i % 2 == 0) {
+      console.log(`This is the even number ${i}`);
     }
-    
   }
-}
+};
 //ArrowEven(50)
 
 //4 prblm
 //Sum of numbers from 1 to 100:
 let ArrowSum = 0;
 const ArrowSumFun = (number) => {
-  for(let i=0; i<=number; i++){
-    ArrowSum = i+ArrowSum
+  for (let i = 0; i <= number; i++) {
+    ArrowSum = i + ArrowSum;
   }
   console.log(ArrowSum);
-}
+};
 //ArrowSumFun(1000)
 
 //5 prblm
@@ -67,11 +65,38 @@ const ArrowSumFun = (number) => {
 
 let ArrowFactorial = 1;
 const ArrowFactorialNumber = (number) => {
-  for(let i=1; i<=number; i++){
-    ArrowFactorial = i * ArrowFactorial
+  for (let i = 1; i <= number; i++) {
+    ArrowFactorial = i * ArrowFactorial;
   }
-  console.log(`The Factorial of ${ArrowFactorial}`)
-}
+  console.log(`The Factorial of ${ArrowFactorial}`);
+};
 //ArrowFactorialNumber(4)
 
+//create calculator
+
+const ArrowCalculator = (a, b, operator) => {
+  let result;
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+     return a - b;
+    case "*":
+      return a*b;
+    case "/":
+      return a/b;
+    default:
+      return "No operator found"
+  }
+};
+//console.log(ArrowCalculator(10, 10, "+"));
+//Reverse Function
+const ArrowReverseFunction = (str) => {
+  let reverse=""
+  for(let char=str.length-1; char>=0; char--){
+    reverse = reverse+str[char]
+  }
+  return reverse
+}
+console.log(ArrowReverseFunction("niraj sakhare"));
 
