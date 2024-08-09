fd79d1403dfabb4ef18913f088aec366cbea5457
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-08-09 12:16:02 +0530

    Array

---
+++ 1.JavaScript/Array.js
@@ -0,0 +1,18 @@
+//JavaScript Array is an object that represents a collection of similar data type
+//Each value(name) will be called as an Element
+//creating array--CRUD--filter in an array--very very important Array Method--Array traversal/Iterating Over Arrays--Searching is an array--how to sort and compare an array
+//🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆
+//Iterable == Object where can use the for-of loop
+//array as object
+//🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆
+//creating Array
+//🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆
+//1.Array in javaScript can be created using the Array constructor or with array literals (square brackets [])
+
+let fruits = new Array("banna","apple","mango")
+//console.log(fruits);
+
+//2.Using array literals
+
+let arrFurits = ["banna", "apple", "mango"];
+console.log(arrFurits)
